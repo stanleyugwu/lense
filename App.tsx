79328a470d70865e52daeba5e90a55e3bd2540e9
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-06-20 05:50:35 +0100

    feat: implement stack navigation and define define some app types

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Text, View } from "react-native";
 import useLoadFonts from "./hooks/useLoadFonts";
 import SplashScreen from 'react-native-splash-screen';
+import Navigation from "./navigation";
 
 export default function App() {
   const isFontsLoaded = useLoadFonts();
@@ -9,8 +9,6 @@ export default function App() {
   SplashScreen.hide(); // now hide native splashscreen, after fonts are loaded
 
   return (
-    <View>
-      <Text>HELLO</Text>
-    </View>
+    <Navigation/>
   );
 }
